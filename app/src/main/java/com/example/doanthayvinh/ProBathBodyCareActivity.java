a8f3557510d1f3f74c9c2c05c8489f45bec77752
TIEN DAT <nguyentiendatx12345@gmail.com>
2022-11-22 13:26:22 +0700

    1

---
+++ app/src/main/java/com/example/doanthayvinh/ProBathBodyCareActivity.java
@@ -0,0 +1,13 @@
+package com.example.doanthayvinh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProBathBodyCareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pro_bath_body_care);
+    }
+}
